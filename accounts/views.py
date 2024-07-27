f77458470333244c88dd181b731c4ca737187169
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-07-27 13:56:09 +0200

    add signup and activate

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.core.mail import send_mail
 
-# Create your views here.
+from .forms import SignupForm,ActivateForm
+from .models import Profile
+from django.contrib.auth.models import User
+
+def signup(request):
+    
+    '''
+    - create new user
+    - stop active this user
+    - send email to this user
+    - return rediect activate.html
+    '''
+    if request.method=='POST':
+        form=SignupForm(request.POST)
+        if form.is_valid():
+            username=form.cleaned_data['username']
+            email=form.cleaned_data['email']
+
+            user=form.save(commit=False)
+            user.is_active=False
+            form.save()    # create new user and create new profile to this user
+            # send email
+
+            profile=Profile.objects.get(user__username=username)
+            send_mail(
+                "Activate Code",
+                f"welcome mr {username}\n pls use this code {profile.code}",
+                "r_mido99@yahoo.com",
+                [email],
+                fail_silently=False,
+            )
+            return redirect(f'/accounts/{username}/activate')
+
+    else:
+        form=SignupForm()
+    return render(request,'accounts/signup.html',{'form':form})
+
+
+
+def activate_code(request,username):
+    
+    '''
+    - active this user
+    - return redirect login
+    '''
+    profile=Profile.objects.get(user__username=username)
+
+    if request.method=='POST':
+        
+        form=ActivateForm(request.POST)
+        if form.is_valid():
+            code=form.cleaned_data['code']
+            if code == profile.code:
+                profile.code=''
+
+                user=User.objects.get(username=username)
+                user.is_active=True
+
+                user.save()
+                profile.save()
+
+                return redirect('/accounts/login')
+
+
+    else:
+        form=ActivateForm()
+
+    return render(request,'accounts/activate_code.html',{'form':form})
--- project/settings.py
+++ project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -133,3 +133,5 @@ MEDIA_ROOT= BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
