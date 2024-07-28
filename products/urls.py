8175eceb50fb447dd8609cc76f626fcf5054bef8
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-07-28 16:12:42 +0200

    add product detail

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Product_List,Product_Detail
+from .views import Product_List,Product_Detail,add_review
 
 urlpatterns = [
     path('',Product_List.as_view()),
     path('<slug:slug>',Product_Detail.as_view()),
+    path('add_review/<slug:slug>',add_review),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import ListView,DetailView
 
-from .models import Product
+from .models import Product,Review,ProductImage
 
 class Product_List(ListView):
     model=Product
@@ -9,3 +9,27 @@ class Product_List(ListView):
 
 class Product_Detail(DetailView):
     model=Product
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["reviews"] = Review.objects.filter(product=self.get_object()).order_by('-id')[:3]
+        context["related"] = Product.objects.filter(brand=self.get_object().brand)
+        context["images"] = ProductImage.objects.filter(product=self.get_object())
+        return context
+
+
+def add_review(request,slug):
+    user=request.user
+    product=Product.objects.get(slug=slug)
+    rate=int(request.POST['rating'])
+    review=request.POST['myreview']
+    Review.objects.create(
+        user=user,
+        product=product,
+        rate=rate,
+        review=review
+    )
+    return redirect(f'/products/{slug}')
+
+
+    
